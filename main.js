4954fdc31bb6b10de59ad4559dc0b7de47addc46
Jonatan Marques <jonatanmarsv@gmail.com>
2022-03-30 18:24:33 -0300

    Neste ponto esta pronto a estrutura no arquivo JS, proximo passo é ir ligar a interface do HTML

--- main.js
+++ main.js
@@ -7,12 +7,39 @@ const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
 const tempClient = {
-    
+    nome: "Jonatan",
+    email: "jonatanmarsv@gmail.com",
+    numero: "54992435656",
+    cidade: "Passo Fundo"
 }    
 
-// CRUD
-const createClient = (client) {
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? []
+const setLocalStorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient))
 
+// CRUD
+
+// CRUD - CREATE
+const createClient = (client) => {
+    const dbClient = getLocalStorage() 
+    dbClient.push(client)
+    setLocalStorage(dbClient)
+}
+
+// CRUD - READ
+const readClient = () => getLocalStorage()
+
+// CRUD - UPDATE
+const updateClient = (client) => {
+    const dbClient = readClient()
+    dbClient[index] = client
+    setLocalStorage(dbClient)
+}
+
+// CRUD - DELETE
+const deleteCliente = (index) => {
+    const dbClient = readClient()
+    dbClient.splice(index, 1)
+    setLocalStorage(dbClient)
 }
 
 
